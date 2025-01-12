3d8b5a010ac17eb13d64783c55301cb2f2cff3a3
justincui177 <chenwec2@uci.edu>
2025-01-12 12:43:56 +0800

    HW1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -13,16 +13,46 @@ public class Player : MonoBehaviour
 
     private void Start ()
     {
-        
+       // Set the initial seed number
+       _numSeedsLeft = _numSeeds;
+       
+       // The initial planted number is 0
+       _numSeedsPlanted = 0;
+       
+       // Update the UI display
+       _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);  
     }
 
     private void Update()
     {
-        
+        // Use WASD or arrow keys to get player input and control horizontal and vertical movement
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+       
+        // Create a new movement vector and update the player position based on speed and time increments
+        Vector3 movement = new Vector3(horizontal, vertical, 0f);
+        _playerTransform.position += movement * _speed * Time.deltaTime;
+
+        // Detect when the space bar is pressed, and if pressed, plant the seed
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            PlantSeed();
+        } 
     }
 
     public void PlantSeed ()
     {
-        
+        if (_numSeedsLeft > 0)
+        {
+            // Spawn plants at the player's current location
+            Instantiate(_plantPrefab, _playerTransform.position, Quaternion.identity);
+            
+            // Reduce the number of remaining seeds and increase the number planted
+            _numSeedsLeft--;
+            _numSeedsPlanted++;
+
+            // Update UI
+            _plantCountUI.UpdateSeeds(_numSeedsLeft, _numSeedsPlanted);
+        } 
     }
 }
